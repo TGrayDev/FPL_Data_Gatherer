534448826648bf2fdd7c30a2f19d8fbcd246bb12
Thomas <thomasgdev@protonmail.com>
2023-02-23 23:35:24 +0000

    Added some comments to main.py

--- main.py
+++ main.py
@@ -6,32 +6,33 @@ from fpl import FPL
 import pyodbc
 
 
-
 async def main():
-
     await UpdateTable()
-    await idealPicks()
 
 
 async def UpdateTable():
     async with aiohttp.ClientSession() as session:
         fpl = FPL(session)
 
+    # Opens connection to database
     conn = pyodbc.connect('Driver={SQL Server};'
-                          'Server=PC08212428;'
-                          'Database=FPL_Players;'
+                          'Server=XXXXXXXX;'  ### ADD OWN SERVER NAME
+                          'Database=XXXXXX;'  ### ADD OWN DATABASE NAME
                           'Trusted_Connection=yes;')
 
     cursor = conn.cursor()
 
     players = await fpl.get_players()
 
+    # Drops the table, and re-makes it in essence updating the database. There's probably a nicer way of doing this,
+    # but I will learn!
     try:
         print("Dropping players table.")
         cursor.execute('DROP TABLE players')
     except:
         print("Players table didn't exist, will now create.")
 
+    # Creates the table
     cursor.execute('''
             CREATE TABLE players (
                 player_id INT IDENTITY,
@@ -44,20 +45,19 @@ async def UpdateTable():
                    ''')
     conn.commit()
 
+    # Just an indicator on how active players are in the FPL game
+    players_count = len(players)
+    print("There are: ", players_count, " available players.")
 
-    playersCount = len(players)
-    print ("There are: ", playersCount, " available players.")
-
-
-
-    for i in range(playersCount):
+    # Adds a row for each player
+    for i in range(players_count):
         player_firstname = str(players[i].first_name)
-
         # For those pesky players with apostrophes in their names, looking at you N'Golo Kante
         player_firstname = player_firstname.replace("'", "''")
         player_secondname = str(players[i].second_name)
         player_secondname = player_secondname.replace("'", "''")
         player_minutes = players[i].minutes
+        # Player cost isn't in a format that users are used to
         player_cost = players[i].now_cost / 10
         player_form = players[i].form
 
@@ -70,14 +70,14 @@ async def UpdateTable():
         conn.commit()
     cursor.close()
 
-
     cursor.commit()
+
 
 if __name__ == "__main__":
     if sys.version_info >= (3, 7):
-         # Python 3.7+
-         asyncio.run(main())
+        # Python 3.7+
+        asyncio.run(main())
     else:
-         # Python 3.6
-         loop = asyncio.get_event_loop()
-         loop.run_until_complete(main())
+        # Python 3.6
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
